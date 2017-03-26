9e862109de2d34b23c48bc8379eef3996d178280
brynjarb15 <brynjarb15@ru.is>
2017-03-26 18:03:41 +0000

    pikachu er sprite sem hleypur og snýst

--- app/scripts/player.js
+++ app/scripts/player.js
@@ -6,7 +6,7 @@ window.Player = (function () {
 	// All these constants are in em's, multiply by 10 pixels
 	// for 1024x576px canvas.
 	var SPEED = 30; // * 10 pixels per second
-	var WIDTH = 5;
+	var WIDTH = 7;
 	var HEIGHT = 5;
 	var INITIAL_POSITION_X = 30;
 	var INITIAL_POSITION_Y = 25;
@@ -19,8 +19,10 @@ window.Player = (function () {
 
 	var Player = function (el, game) {
 		this.el = el;
+		this.child = this.el.find('.pikachu');
 		this.game = game;
 		this.pos = { x: 0, y: 0 };
+		this.rotationDegree = 0;
 	};
 
 	/**
@@ -35,6 +37,10 @@ window.Player = (function () {
 
 		if (Controls.didJump()) {
 			this.pos.y -= delta * SPEED*5;
+			this.rotationDegree = -25;
+		}
+		else if(this.rotationDegree < 90) {
+			this.rotationDegree += 1;
 		}
 		
 		this.pos.y += delta * SPEED*0.2; // ef þessi lína er með þá ferðu rólega niður
@@ -54,6 +60,7 @@ window.Player = (function () {
 		this.checkCollisionWithBounds();
 
 		// Update UI
+		this.child.css('transform', 'rotate(' + this.rotationDegree + 'deg)');
 		this.el.css('transform', 'translateZ(0) translate(' + this.pos.x + 'em, ' + this.pos.y + 'em)');
 	};
 
